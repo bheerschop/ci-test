f73defe193be8f4c0320090ca8b4808545994634
Bas Heerschop <basheerschop@gmail.com>
2018-08-10 19:32:55 +0200

    Added test project

---
+++ PeriodLibraryTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PeriodLibraryTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
